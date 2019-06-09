86770fab8d3a4a65deff5daea38fa21d2598f1fc
James-Alex Matthews <vamtiger@vamtiger.com>
2019-06-09 19:27:52 +0200

    vamtiger-node-typescript-commit

---
+++ source/json-ld.ts
@@ -0,0 +1,32 @@
+const jsonLd = {
+    '@context': 'http://schema.org',
+    '@type': 'Organization',
+    name: 'Infers Group',
+    description: `
+        Infers Group was set up in 2015 as a knowledge management company that connects
+        societies and knowledge producers/institutions to communicate, engage and establish each other’s
+        relevance for better participation in knowledge economies in Africa.
+
+        The company operated through its Gaborone and Cape Town offices to bring together Africa’s brilliant
+        minds, creating ecosystems that allow the ambition of knowledge economies to become reality.
+
+        In 2018 Infers Group re-thought its strategy to become proactive by participating in management and
+        coordination of networks and information to awaken talents, imagination, and lifelong learning in
+        knowledge economies.
+        The strategy is designed to facilitate networks that cross-pollinate new ideas and practices to catalyse new forms of entrepreneurship through Africa’s unique cultural
+        capital.
+
+        The organisation has developed a network-centric strategy that will enable enterprises to traject
+        into businesses capable of transitioning Botswana into a knowledge based economy facilitated by
+        effectively managed projects, intellectual property, economies of scale, monitoring and evaluation
+        and advanced services, and linking knowledge producers in all sectors with policy, business
+        community, the public and various community groups to advance a Botswana economy that is
+        translatable into SADC and global markets.
+    `,
+    url: 'http://infersgroup.com',
+    email: 'mailto:info@infersgroup.com'
+};
+
+export default [
+    jsonLd
+];
