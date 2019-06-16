1f27c318fd49bd9ea4a9f9155c0b2e214a544879
James-Alex Matthews <vamtiger@vamtiger.com>
2019-06-16 21:59:44 +0200

    test

---
+++ build/json-ld.d.ts
@@ -0,0 +1,9 @@
+declare const _default: {
+    '@context': string;
+    '@type': string;
+    name: string;
+    description: string;
+    url: string;
+    email: string;
+}[];
+export default _default;
